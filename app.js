60193f4c7cf62d497f5f688162a6cc8fd370726e
Mahesh Dadwal <maheshdadwal07@gmail.com>
2026-03-14 16:35:01 +0530

    verify authentication APIs using Postman

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const app = express();
 app.use(express.json());
 
 const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "HireHub API Running" });
---
+++ routes/user.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware = require("../middleware/auth.middleware");
+
+router.get("/me", authMiddleware, (req, res) => {
+
+  res.json({
+    message: "Authenticated user",
+    user: req.user
+  });
+
+});
+
+module.exports = router;
